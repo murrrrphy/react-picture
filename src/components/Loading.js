694d0555429f4f7c63d8a1119c463c0dec303dfa
murphy <987127314@qq.com>
2021-01-19 14:41:37 +0800

    修改ReadMe,为loading组件增加样式

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Spin } from 'antd';
+import styled from 'styled-components';
+
+const SpinWrapper = styled(Spin)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
 
 function Loading() {
   return (
-    <div>Loading...</div>
+    <SpinWrapper tip="Loading..."/>
   )
 }
 
