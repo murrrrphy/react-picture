6f6c5d8bda48d21d382b4c7e2d1229cafafe0ac4
murphy <987127314@qq.com>
2021-01-19 17:01:35 +0800

    退出登录按钮样式和导航栏样式

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:murrrrphy/react-picture-website.git
+git push -f git@github.com:murrrrphy/react-picture-website.git master
 
 cd -
--- src/components/Header.js
+++ src/components/Header.js
@@ -17,27 +17,83 @@ const HeaderWrapper = styled.header`
   }
 `;
 
+const User =styled.div`
+  display: flex;
+  align-items: center;
+`
+
 const LinkWrapper = styled(NavLink)`
+  position: relative;
+  display: inline-block;
   color: white;
-  padding: 10px 15px;
+  padding: 20px 20px;
+  transition: transform 0.3s;
+  transform-origin: 50% 0;
+  transform-style: preserve-3d;
   &:hover{
     background: #262e37;
+    transform: rotateX(90deg) translateY(-22px);
     color: white;
   }
   &.active {
-    padding: 10px 15px 6px 15px;
-    background: #262e37;
-    border-bottom: 4px solid #4a88c7;
+    background: #0965a0;
+    transform: rotateX(90deg) translateY(-22px);
+  }
+  &::before {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #0965a0;
+    content: attr(data-hover);
+    transition: background 0.3s;
+    transform: rotateX(-90deg);
+    transform-origin: 50% 0;
   }
 `;
 
 const Login = styled.div`
   margin-left: auto;
   color: white;
+  strong {
+    font-size: 16px;
+  }
 `;
 
 const ButtonWrapper = styled(Button)`
   margin-left: 10px;
+  border-radius: 16px;
+`;
+
+const Layout = styled(ButtonWrapper)`
+  background: #3b4754;
+  padding: 4px 36px;
+  border-radius: 16px;
+  border: none;
+  display: flex;
+  align-items: center;
+  position: relative;
+  .icon {
+    position: absolute;
+    fill: white;
+    height: 1em;
+    width: 1em;
+    opacity: 0;
+    left: 96px;
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+  &:hover{
+    background: #e81123;
+    .icon {
+      opacity: 1;
+      left: 100px;
+    }
+  }
 `;
 
 const  Header = observer(() => {
@@ -65,15 +121,15 @@ const  Header = observer(() => {
   return (
     <HeaderWrapper>
       <Icon name="M"/>
-      <LinkWrapper exact to="/" activeClassName="active">首页</LinkWrapper>
-      <LinkWrapper to="/history" activeClassName="active">历史记录</LinkWrapper>
-      <LinkWrapper to="/about" activeClassName="active">关于我</LinkWrapper>
+      <LinkWrapper exact to="/" activeClassName="active" data-hover="首页">首页</LinkWrapper>
+      <LinkWrapper to="/history" activeClassName="active" data-hover="历史记录">历史记录</LinkWrapper>
+      <LinkWrapper to="/about" activeClassName="active" data-hover="关于我">关于我</LinkWrapper>
       <Login>
         {UserStore.currentUser?
-          <>
-            Hello,{UserStore.currentUser.attributes.username}
-            <ButtonWrapper type="primary" onClick={handleLogout}>注销</ButtonWrapper>
-          </>
+          <User>
+            <strong>Hello,{UserStore.currentUser.attributes.username}</strong>
+            <Layout type="primary" onClick={handleLogout}>退出登录<Icon name="right"/></Layout>
+          </User>
           :
           <>
             <ButtonWrapper type="primary" onClick={handleLogin}>
